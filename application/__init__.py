4148f95cc7492aba66f61d3eb4378f8b082c73a8
ahsansabir30 <ahsansabir30@gmail.com>
2022-08-02 16:05:17 +0000

    updating init.py file

--- application/__init__.py
+++ application/__init__.py
@@ -4,10 +4,10 @@ from os import getenv
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = '4156e3619c604634022d31aba05b3060'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-#app.config['SECRET_KEY']= getenv('SECRET_KEY')
-#app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URI")
+#app.config['SECRET_KEY'] = '4156e3619c604634022d31aba05b3060'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY']= getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URI")
 
 db = SQLAlchemy(app)
 
